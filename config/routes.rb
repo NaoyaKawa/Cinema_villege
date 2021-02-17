5e91c5fa3d9d018ce287337f18ab2b9dff158102
NaoyaKawa <reventon27urobolos@gmail.com>
2021-02-17 03:14:47 +0000

    change layout

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,6 @@ Rails.application.routes.draw do
     resources :theaters
   end
   
-
   get   'inquiry'         => 'inquiry#index'     # 入力画面
   post  'inquiry/confirm' => 'inquiry#confirm'   # 確認画面
   post  'inquiry/thanks'  => 'inquiry#thanks'    # 送信完了画面
