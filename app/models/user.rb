4cb440dfe487fb5449860a91199bf506a9212dcc
NaoyaKawa <reventon27urobolos@gmail.com>
2021-02-23 15:57:27 +0000

    fix syntax in magazine new

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,4 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   attachment :image
-  validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 20 }
 end
