4ec138f0c2b7a4ebd76e1883421f37e2c8aff8a8
NaoyaKawa <reventon27urobolos@gmail.com>
2021-02-24 08:54:22 +0000

    fix migrate

--- db/migrate/20210219070615_add_theater_id_to_theater_tag.rb
+++
@@ -1,6 +0,0 @@
-class AddTheaterIdToTheaterTag < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :theater_tags, :theater, foreign_key: true
-    add_reference :theater_tags, :tag, foreign_key: true
-  end
-end
